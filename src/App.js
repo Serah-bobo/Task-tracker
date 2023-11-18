65d987801994276110e56da23c8333c11f0f2ecd
Serah-bobo <serahw@cragroup.co.ke>
2023-11-18 20:31:19 +0300

    my projects

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Profile from "./components/Profile";
+
+
+
+  export default function MyApp() {
+    
+      
+        return (
+            <div>
+            <Profile/>
+             
+            </div>
+          );
+        }
+        
+        
